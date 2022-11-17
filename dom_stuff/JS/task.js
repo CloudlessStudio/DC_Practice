d721208899288e40272745c5df8198313ec6f669
Konrad <konradQPL@gmail.com>
2022-11-17 17:23:47 -0600

    added exception for empty tasks

--- dom_stuff/JS/task.js
+++ dom_stuff/JS/task.js
@@ -1,36 +1,39 @@
 function addTask(){
     const taskText = document.getElementById("inputTask").value;
-    const pendingDiv = document.getElementById("pendingSection");
-    const completedDiv = document.getElementById("completedSection")
-    const pendingChild = document.createElement("div"); 
-    pendingChild.classList.add("taskName");
-    const completeCheck = document.createElement("INPUT");
-    completeCheck.classList.add("boxStyle");
-    const pendingText = document.createElement("p")
-    completeCheck.setAttribute("type", "checkbox");
-    const removeButton = document.createElement("button");
-    removeButton.innerText = "REMOVE";
-    pendingText.innerText = taskText;
-    pendingChild.append(completeCheck);
-    pendingChild.append(pendingText);
-    pendingChild.append(removeButton);
-    pendingDiv.append(pendingChild);
-    document.getElementById("inputTask").value = "";
-    removeButton.addEventListener("click", event =>{
-        pendingChild.remove();
-    })
-    completeCheck.addEventListener("change",event =>{
-        if(event.target.checked){
-            completedDiv.append(pendingChild);
-            console.log("CHECKED");
-        }
-        else{
-            pendingDiv.append(pendingChild);
-            console.log("UNCHECKED");
-        }
-    })
-
-
-    console.log("added stuff");
+    if(taskText ==''){
+        alert("No task was added!");
+    }
+    else{
+        const pendingDiv = document.getElementById("pendingSection");
+        const completedDiv = document.getElementById("completedSection")
+        const pendingChild = document.createElement("div"); 
+        pendingChild.classList.add("taskName");
+        const completeCheck = document.createElement("INPUT");
+        completeCheck.classList.add("boxStyle");
+        const pendingText = document.createElement("p")
+        completeCheck.setAttribute("type", "checkbox");
+        const removeButton = document.createElement("button");
+        removeButton.innerText = "REMOVE";
+        pendingText.innerText = taskText;
+        pendingChild.append(completeCheck);
+        pendingChild.append(pendingText);
+        pendingChild.append(removeButton);
+        pendingDiv.append(pendingChild);
+        document.getElementById("inputTask").value = "";
+        removeButton.addEventListener("click", event =>{
+            pendingChild.remove();
+        })
+        completeCheck.addEventListener("change",event =>{
+            if(event.target.checked){
+                completedDiv.append(pendingChild);
+                console.log("CHECKED");
+            }
+            else{
+                pendingDiv.append(pendingChild);
+                console.log("UNCHECKED");
+            }
+        })
+        console.log("added stuff");
+    }
 
 }
